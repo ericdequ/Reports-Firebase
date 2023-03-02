9c27b2557b3f992923355535aa33b1112e0914a2
ericdequ <dequevedo.eric@ufl.edu>
2023-03-02 14:49:56 -0500

    change

--- src/js/components/IFrame/iframe.js
+++ src/js/components/IFrame/iframe.js
@@ -14,25 +14,25 @@ const IFrame = () => {
   const onMessage = (event) => {
 
    console.log("GOT A MESSAGE")
-   if(allowedOrigins.includes(event.orgin)){
+   
+    console.log("Got parent message");
 
-    console.log("Got parent message")
     window.parent.postMessage('Recieved message from parent ', '*');
 
-      const accessToken = event.data.access_token;
+    const accessToken = event.data.access_token;
 
-      window.parent.postMessage('Parent',accessToken);
+    window.parent.postMessage('Parent',accessToken);
 
-      console.log('Parent',accessToken);
+    console.log('Parent',accessToken);
 
       // Call an external API to validate the access token
       // In this example, we just assume it's valid if it's not empty
-      const tokenIsValid = (accessToken !== "");
+    const tokenIsValid = (accessToken !== "");
 
-      setIsValidToken(tokenIsValid);
+    setIsValidToken(tokenIsValid);
     
 
-  }
+  
   };
 
   window.addEventListener('message', onMessage);
