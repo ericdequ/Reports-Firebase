c84802c5ece7cef87f17c93923ad809254d71829
ericdequ <dequevedo.eric@ufl.edu>
2023-03-02 13:54:13 -0500

    iframe

--- src/js/components/IFrame/iframe.js
+++ src/js/components/IFrame/iframe.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import AppContainer from '../../containers/AppContainer';
 
+const allowedOrigins = [
+  "https://crm-reports-94d86.web.app/",
+  "https://crm-reports-94d86.firebaseapp.com/",
+  "http://localhost:3000/",
+  "http://192.168.0.235:3000/",
+];
+
 const IFrame = () => {
   const [isValidToken, setIsValidToken] = useState(false);
 
   const onMessage = (event) => {
-   if(event.source == parent.window){
+   if(allowedOrigins.includes(event.orgin)){
 
     console.log("Got parent message")
     window.parent.postMessage('Recieved message from parent ', '*');
@@ -13,6 +20,7 @@ const IFrame = () => {
       const accessToken = event.data.access_token;
 
       window.parent.postMessage('Parent',accessToken);
+
       console.log('Parent',accessToken);
 
       // Call an external API to validate the access token
