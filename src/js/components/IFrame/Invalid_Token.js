6cda07e1036e6e1bfc534704a808343dae1a3f84
ericdequ <dequevedo.eric@ufl.edu>
2023-03-07 10:19:41 -0500

    invalid page

---
+++ src/js/components/IFrame/Invalid_Token.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InvalidAccessToken = () => {
+  return (
+    <div>
+      <h1>Error: Invalid Access Token</h1>
+      <p>The access token provided is invalid. Please try again with a valid access token.</p>
+    </div>
+  );
+};
+
+export default InvalidAccessToken;
--- src/js/components/IFrame/iframe.js
+++ src/js/components/IFrame/iframe.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AppContainer from '../../containers/AppContainer';
+import InvalidAccessToken from './Invalid_Token';
 
 const allowedOrigins = [
   "https://crm-reports-94d86.web.app/",
@@ -8,30 +9,34 @@ const allowedOrigins = [
   "http://192.168.0.235:3000/",
 ];
 
+/* make a call to CRM firebase auth or copy or the auth */
+const checkIsValid = ()=>{
+   
+}
+
 const IFrame = () => {
   const [isValidToken, setIsValidToken] = useState(false);
 
   const onMessage = (event) => {
 
   console.log("GOT A MESSAGE")
-
   console.log(event.data)
-
   console.log(event.data.access_token)
-
+  console.log(event.allowedOrigins)
   console.log("Got parent message");
-
   window.parent.postMessage('Recieved message from parent ', '*');
+
+
 
   const accessToken = event.data.access_token;
 
   window.parent.postMessage('Parent', accessToken);
 
-    console.log('Parent',accessToken);
+  console.log('Parent',accessToken);
 
       // Call an external API to validate the access token
       // In this example, we just assume it's valid if it's not empty
-    const tokenIsValid = (accessToken !== "");
+   const tokenIsValid = (accessToken !== "");
 
     setIsValidToken(tokenIsValid);
       
@@ -44,13 +49,11 @@ const IFrame = () => {
       {isValidToken ? (
         // Render your application content here
         <>
-          
-          <AppContainer /><h1>Valid access token</h1></>
+          <AppContainer /></>
       ) : (
         // Render your application content here
         <>
-          
-          <AppContainer /><h1>invalid access token</h1></>
+          <InvalidAccessToken /></>
       )}
     </div>
   );
